5fe4249a4c4264ea394a9f8ef9f468b64aaf6519
PrynsTag <201812311@fit.edu.ph>
2021-04-23 19:31:05 +0800

    added functions for writing csv and markdown.

--- github_crawler/spiders/github_crawl.py
+++ github_crawler/spiders/github_crawl.py
@@ -10,6 +10,27 @@ from scrapy.crawler import CrawlerProcess
 from info import *
 
 
+def write_to_csv(filename, column_header, data):
+    file_exists = os.path.exists(f"{filename}.csv")
+
+    with open(f"{filename}.csv", 'a') as file:
+        writer = csv.writer(file)
+        if not file_exists:
+            writer.writerow(column_header)
+
+        writer.writerow(data)
+
+
+def write_to_md(filename, data):
+    title, desc, updated, language, link = data
+
+    with open(f"{filename}.md", 'a') as file:
+        file.write(f"# [{title}]({link})\n")
+        file.write(f"###### Language: {language}\n")
+        file.write(f"###### Updated: {updated}\n")
+        file.write(f"### {desc.strip()}\n")
+
+
 class GithubCrawlSpider(scrapy.Spider):
     name = 'github-crawl'
     start_urls = ["https://github.com/login"]
@@ -28,32 +49,28 @@ class GithubCrawlSpider(scrapy.Spider):
         yield response.follow(response.url + f"/{login}?tab=repositories&type=source", callback=self.parse_repo)
 
     def parse_repo(self, response):
-        filename = f"[{strftime('%Y-%m-%d', gmtime())}] github_repo.csv"
-        file_exists = os.path.exists(filename)
+        filename = f"[{strftime('%Y-%m-%d', gmtime())}] github_repo"
+        column_header = ["Title", "Description", "Updated", "Language", "Link"]
 
-        with open(filename, 'a+') as file:
-            writer = csv.writer(file)
+        repo_details = response.css("#user-repositories-list > ul > li > div.col-10.col-lg-9.d-inline-block")
+        for repo in repo_details:
+            title = repo.css("div.d-inline-block.mb-1 > h3 > a::text").get().strip()
+            desc = repo.css("p[itemprop=description]::text").get()
 
-            if not file_exists:
-                writer.writerow(["Name", "Description", "Updated", "Language", "Link"])
+            date = repo.css("div.f6.color-text-secondary.mt-2 > relative-time::attr(datetime)").get()
+            updated = datetime.now() - datetime.strptime(date, '%Y-%m-%dT%H:%M:%SZ')
 
-            repo_details = response.css("#user-repositories-list > ul > li > div.col-10.col-lg-9.d-inline-block")
-            for repo in repo_details:
-                title = repo.css("div.d-inline-block.mb-1 > h3 > a::text").get().strip()
-                desc = repo.css("p[itemprop=description]::text").get()
+            language = repo.css(
+                "div.f6.color-text-secondary.mt-2 > span > span[itemprop=programmingLanguage]::text").get()
 
-                date = repo.css("div.f6.color-text-secondary.mt-2 > relative-time::attr(datetime)").get()
-                updated = datetime.now() - datetime.strptime(date, '%Y-%m-%dT%H:%M:%SZ')
+            url = "https://github.com" + repo.css("div.d-inline-block.mb-1 > h3 > a::attr(href)").get()
 
-                language = repo.css(
-                    "div.f6.color-text-secondary.mt-2 > span > span[itemprop=programmingLanguage]::text").get()
-
-                url = "https://github.com" + repo.css("div.d-inline-block.mb-1 > h3 > a::attr(href)").get()
-
-                if desc is not None:
-                    writer.writerow([title, desc.strip(), updated, language, url])
-                else:
-                    writer.writerow([title, 'No description', updated, language, url])
+            if desc is not None:
+                write_to_csv(filename, column_header, [title, desc.strip(), updated, language, url])
+                write_to_md(filename, [title, desc.strip(), updated, language, url])
+            else:
+                write_to_csv(filename, column_header, [title, "No Description", updated, language, url])
+                write_to_md(filename, [title, "No Description", updated, language, url])
 
         pagination = response.css(
             "#user-repositories-list > div.paginate-container > div[data-test-selector=pagination] > a")
